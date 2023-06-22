365ce3adeae9606644dd52ec0aa30c30f76b8018
hildelio <hildelio@gmail.com>
2023-06-22 11:24:40 -0300

    Trybe Crafts :factory_worker: Exercício 04

--- src/04-calculateTotalPrice.ts
+++ src/04-calculateTotalPrice.ts
@@ -1,3 +1,15 @@
-// export default function calculateTotalPrice() {
-//   // insira a solução
-// }
+import { Product } from './02-insertProducts';
+
+export type Item = {
+  product: Product,
+  quantity: number,
+  phrase: string,
+  brand: string
+};
+
+export default function calculateTotalPrice(item: Item[]): number {
+  if (item.length === 0) {
+    return 0;
+  }
+  return +item.reduce((acc, i) => acc + i.product.price * i.quantity, 0).toFixed(2);
+}
