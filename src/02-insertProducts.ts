1a72c33d42d59b658c85f6b66c01ce5d035192b0
hildelio <hildelio@gmail.com>
2023-06-22 10:59:35 -0300

    Trybe Crafts :factory_worker: Exercício 02

--- src/02-insertProducts.ts
+++ src/02-insertProducts.ts
@@ -1,3 +1,15 @@
-// export default function insertProducts() {
-//   // insira a solução
-// }
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  brands: string[];
+};
+
+export default function insertProducts(brandsList: string[], product: Product): string {
+  const verifyBrand = product.brands.every((brand) => brandsList.includes(brand));  
+  if (!verifyBrand) {
+    return 'Seu produto contém marcas indisponíveis.';
+  }
+  return `${product.name} adicionado(a) com o preço de R$ ${product.price}!`;
+}
